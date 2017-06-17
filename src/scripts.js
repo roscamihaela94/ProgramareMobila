48c82bece791a55b995fbe58d5126002409c169a
Unknown <roscamihaela94@gmail.com>
2017-06-17 16:12:51 -0700

    Am incarcat imaginea de background si am desenat-o pe canvas.

--- src/scripts.js
+++ src/scripts.js
@@ -1,13 +1,25 @@
 var joc = {
 	canvas:document.createElement("canvas"),
+	background: document.getElementById("background"),
 creazaJoc: function() {
  		this.canvas.width = screen.width ;
  		this.canvas.height = 270;
  		this.context = this.canvas.getContext("2d");
  		document.body.insertBefore(this.canvas, document.body.childNodes[0]);
 
- 	}
+ 	},
+ 	stergeCanvas: function(){
+ 		this.context.clearRect(0, 0 , this.canvas.width, this.canvas.height);
+ 	},
+ 	updateBackround:function(){
+ 		var ptrn = this.context.createPattern(background, 'repeat') // Aici se creeaza din imaginea un patern si o repeta pe axa Ox
+ 		this.context.fillStyle = ptrn;
+ 		this.context.fillRect(0, 0, this.canvas.width, this.canvas.height);
+  }
  }
  var creazaJoc = function (){
  	joc.creazaJoc();
+ 	joc.updateBackround();
+
  }
+
