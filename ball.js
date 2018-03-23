3d446dfa30b066d9ff5d8fd1fc63d465c971f646
roscamihaela94 <roscamihaela94@gmail.com>
2018-03-23 18:21:52 +0200

    Bila update

---
+++ ball.js
@@ -0,0 +1,24 @@
+
+document.getElementById(id = "id_business_level").innerHTML = "Business version = 2018.03.23.02";
+
+var stare = {y:0, directie: 1};
+setInterval(deseneaza, 30, stare);
+
+function deseneaza(x)
+{
+	var t = document.getElementById("id_transform");
+	t.setAttribute("translation", "0 " + x.y + " 0");
+	x.y += x.directie;
+ 	
+ 	if (x.y > 10 )
+
+ 		x.directie = -1;
+
+ 	else {
+
+ 		if(x.y < 0)
+
+ 			x.directie = 1;
+ 	}
+ 		
+}
